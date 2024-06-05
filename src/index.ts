8c5d85da7a67e20a1f24dc4ff3ced03a6a34e3b0
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-05 14:15:18 -0500

    It works if you do two request 😂

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,23 @@ export default {
 		if (request.method === 'GET') {
 			console.log('Fetching the artifact');
 
-			const file = await fetch('https://api.github.com/repos/Hacksore/overlayed/actions/artifacts/1569393086/zip', {
-				method: 'GET',
-				redirect: 'follow',
+			console.log(env.GITHUB_TOKEN.slice(-5));
+			const response = await fetch('https://api.github.com/repos/Hacksore/overlayed/actions/artifacts/1569393086/zip', {
 				headers: {
 					Accept: 'application/vnd.github+json',
 					Authorization: `Bearer ${env.GITHUB_TOKEN}`,
 					'X-GitHub-Api-Version': '2022-11-28',
 					'User-Agent': 'overlayed-updater v1',
 				},
-			}).then((res) => res.arrayBuffer());
+			});
+			console.log(response);
 
-			console.log(file.byteLength);
-			return new Response(file);
+			const fileResponse = await fetch(response.url);
+
+			// save to file
+			console.log('Saving to file', (await fileResponse.arrayBuffer()).byteLength);
+
+			return new Response();
 		}
 
 		return new Response('No');
