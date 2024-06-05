934eba12864c901464097efd8223c8938f824aeb
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-05 14:38:54 -0500

    more stuffz

--- src/working.js
+++ src/working.js
@@ -1,6 +1,6 @@
 console.log('Fetching the artifact');
 
-const file = await fetch('https://api.github.com/repos/Hacksore/overlayed/actions/artifacts/1569393086/zip', {
+const res = await fetch('https://api.github.com/repos/Hacksore/overlayed/actions/artifacts/1569393086/zip', {
 	method: 'GET',
 	redirect: 'follow',
 	headers: {
@@ -9,6 +9,13 @@ const file = await fetch('https://api.github.com/repos/Hacksore/overlayed/action
 		'X-GitHub-Api-Version': '2022-11-28',
 		'User-Agent': 'overlayed-updater v1',
 	},
-}).then((res) => res.arrayBuffer());
+});
 
-console.log(file.byteLength);
+console.log(res)
+
+console.log('status', res.status);
+console.log('signedUrl', res.url);
+
+const arrayBuffer = await res.arrayBuffer();
+
+console.log(arrayBuffer.byteLength);
