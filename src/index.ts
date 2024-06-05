c2b049f5a9f592d9671b085fb63ef1e0a40917f7
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-05 14:47:31 -0500

    Single fetch to repro it still

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ export default {
 		if (request.method === 'GET') {
 			console.log('Fetching the artifact');
 
-			console.log(env.GITHUB_TOKEN.slice(-5));
 			const response = await fetch('https://api.github.com/repos/Hacksore/overlayed/actions/artifacts/1569393086/zip', {
 				headers: {
 					Accept: 'application/vnd.github+json',
@@ -18,12 +17,14 @@ export default {
 			});
 			console.log(response);
 
-			const fileResponse = await fetch(response.url);
-
 			// save to file
-			console.log('Saving to file', (await fileResponse.arrayBuffer()).byteLength);
+			const size = (await response.arrayBuffer()).byteLength;
 
-			return new Response();
+			return new Response(
+				JSON.stringify({
+					size,
+				}),
+			);
 		}
 
 		return new Response('No');
